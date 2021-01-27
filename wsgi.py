b0f2a6a9dfc564ad9112a77c580e11556b72c7db
Maksymilian Jodłowski <10886532+ravensiris@users.noreply.github.com>
2021-01-27 20:02:20 +0100

    Before flask-restx migration
    add Procfile
    
    add runtime.txt
    
    disable talisman for test
    
    test procfile log-level
    
    add gunicorn to reqs
    
    Test heroku
    
    Remove __main__ from wsgi.py
    
    Fix __main__
    
    Test talisman off
    
    Talisman on inline fix
    
    Swagger CSP fix
    
    Swagger CSP fix2
    
    Swagger CSP fix3

--- wsgi.py
+++ wsgi.py
@@ -7,11 +7,29 @@ from flask_talisman import Talisman
 from flask_cors import CORS
 
 app = Flask(__name__, static_url_path = "")
+
+# Fix flaskrestplus' inline CSS
+# https://stackoverflow.com/questions/58610678/flask-talisman-breaks-flask-restplus-swagger-documentation
+SWAGGER_CSP = {
+    "script-src": ["'self'", "'unsafe-inline'"],
+    "style-src": ["'self'", "'unsafe-inline'"]
+}
+
 Talisman(app)
 CORS(app)
 
+app.config["RESTPLUS_MASK_SWAGGER"] = False
+app.register_blueprint(api_blueprint)
+
+app.view_functions["api.specs"].talisman_view_options = {
+    "content_security_policy": SWAGGER_CSP
+}
+app.view_functions["api.Authentication_authorize_token"].talisman_view_options = {
+    "content_security_policy": SWAGGER_CSP
+}
+app.view_functions["api.Timetable_lesson_units"].talisman_view_options = {
+    "content_security_policy": SWAGGER_CSP
+}
 
 if __name__ == "__main__":
-    app.config["RESTPLUS_MASK_SWAGGER"] = False
-    app.register_blueprint(api_blueprint)
-    app.run()
+    app.run(debug=True)
